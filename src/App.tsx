bda7f468fb53da71353b7f06c8fe9244f7a1ad50
xhuriken <celestin@honvault.com>
2026-09-07 16:51:55 +0200

    Tailwind Installed ! i swear it's the last commit pushed in main (or not)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>Liste des posts</h1>
       <PostList />
+
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </main>
   );
 }
